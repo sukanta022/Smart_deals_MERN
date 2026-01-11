788cde5e44c342ad18e07cb88993178a3220b3f3
sukanta022 <sukantacb20@gmail.com>
2026-01-11 12:23:01 +0600

    added crud related fundamentals in server site

--- smart-deals-server/index.js
+++ smart-deals-server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -29,7 +29,55 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    
+    const db = client.db('smart_db')
+    const productsCollection = db.collection('products')
+
+    //see all products
+    app.get('/products', async(req,res) => {
+      const cursor = productsCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    //to see specific product
+    app.get('/products/:id', async(req,res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await productsCollection.findOne(query)
+      res.send(result)
+    })
+
+    //create new product
+    app.post('/products', async(req,res) => {
+      const newProduct = req.body;
+      const result = await productsCollection.insertOne(newProduct)
+      res.send(result)
+    })
+
+    //delete product
+    app.delete('/products/:id', async(req,res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await productsCollection.deleteOne(query)
+      res.send(query)
+    })
+
+    //update product
+    app.patch('/products/:id', async(req,res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const updateProduct = req.body
+      const update = {
+        $set: {
+          name : updateProduct.name,
+          price : updateProduct.price
+        }
+      }
+      const result = await productsCollection.updateOne(query, update)
+      res.send(result)
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
